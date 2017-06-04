c44dd519383447e3a2cb315e09c5736636000bc5
benji <benji.b.ramirez@gmail.com>
2017-06-04 14:40:17 -0700

    init commit, basic flask app, basic mnist keras model, need to connect them

---
+++ model/mnist_load.py
@@ -0,0 +1,67 @@
+# Benjamin Ramirez Jun 3, 2017
+# loading saved mnist model and evaluating
+import numpy as np
+import os
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import model_from_json
+from keras import backend as K
+
+
+# modules for loading images
+from PIL import Image
+import requests
+from io import BytesIO
+
+url = "http://khanhxnguyen.com/wp-content/uploads/2017/03/mnist-2.png"
+response = requests.get(url)
+image = Image.open(BytesIO(response.content))
+image = image.resize((28, 28))
+image = image.convert(mode="L")
+image.show()
+
+np_image = np.array(image)
+print(np_image.shape)
+# an image url for the number 1
+# https://cdn1.thehunt.com/app/public/system/note_images/2978532/note_view/87c3c5b0b9781ea50e877c049cd1e980.png
+
+K.set_image_dim_ordering('th')
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+# again, this section is taken from Jason Brownlee's tutorial on saving/loading keras models
+# http://machinelearningmastery.com/save-load-keras-deep-learning-models/
+# loading json and creating model from saved files
+json_file = open('./model_saves/mnist_cnn_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights('./model_saves/mnist_cnn_model.h5')
+print("Loaded model from disk")
+
+# evaluate model and compare results to trained model:
+loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+test_input_image = X_test[0]
+# print(test_input_image)
+expanded_dim_img = np.expand_dims(np_image, axis=0) /255
+# print(expanded_dim_img)
+output_test = loaded_model.predict(np.expand_dims(expanded_dim_img, axis=0))
+print("Prediction for X[0]:", np.argmax(output_test))
+
+scores = loaded_model.evaluate(X_test, y_test, verbose=0)
+print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+
